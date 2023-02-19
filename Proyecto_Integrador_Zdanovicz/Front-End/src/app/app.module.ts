c331afc7319b26fb1a81233b22186fd61b5439d5
Zdanovicz Nicolas <zdanovicz02@gmail.com>
2023-02-19 11:57:36 -0300

    acerca-de 19-02

--- Proyecto_Integrador_Zdanovicz/Front-End/src/app/app.module.ts
+++ Proyecto_Integrador_Zdanovicz/Front-End/src/app/app.module.ts
@@ -19,6 +19,11 @@ import { LoginComponent } from './components/login/login.component';
 import { interceptorProvider } from './service/interceptor-service';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
+import { NewEducacionComponent } from './components/educacion/new-educacion.component';
+import { EditEducacionComponent } from './components/educacion/edit-educacion.component';
+import { EditSkillComponent } from './components/hys/edit-skill.component';
+import { NewSkillComponent } from './components/hys/new-skill.component';
+import { EditAcercaDeComponent } from './components/acerca-de/edit-acerca-de.component';
 
 
 @NgModule({
@@ -37,6 +42,11 @@ import { EditExperienciaComponent } from './components/experiencia/edit-experien
     LoginComponent,
     NewExperienciaComponent,
     EditExperienciaComponent,
+    NewEducacionComponent,
+    EditEducacionComponent,
+    EditSkillComponent,
+    NewSkillComponent,
+    EditAcercaDeComponent,
   ],
   imports: [
     BrowserModule,
